313dc3b6c17f3a5adaf2a78418f8337326107646
YoungH66 <rladudgns2222@gmail.com>
2024-03-24 22:14:50 +0900

    0321sort연습

--- algorithmTest/src/main/java/TempWork.java
+++ algorithmTest/src/main/java/TempWork.java
@@ -1,5 +1,18 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class TempWork {
     public static void main(String[] args) {
-        System.out.println("Stay");
+        Scanner sc = new Scanner(System.in);
+        int cap = sc.nextInt();
+        int[] arr = new int[cap];
+        int n = 0;
+        do{
+            arr[n++] = sc.nextInt();
+        }while(n != cap);
+
+        Arrays.sort(arr);
+        for(int i = 0; i < cap; i++)
+            System.out.println(arr[i]);
     }
 }
