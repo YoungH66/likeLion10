8e876c39f60f94b74074934cf8063b90d6b50b08
YoungH66 <rladudgns2222@gmail.com>
2024-03-31 20:20:37 +0900

    0331

---
+++ testtest/src/main/java/test/test0331.java
@@ -0,0 +1,8 @@
+package test;
+
+public class test0331 {
+    public static void main(String[] args) {
+        System.out.println("last day in march");
+        System.out.println("so fast to catch");
+    }
+}
