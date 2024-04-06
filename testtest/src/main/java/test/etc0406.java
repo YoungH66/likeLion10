54c46516c3570ce6589ff787eb9c59ede4121267
YoungH66 <rladudgns2222@gmail.com>
2024-04-06 20:28:40 +0900

    0406

---
+++ testtest/src/main/java/test/etc0406.java
@@ -0,0 +1,13 @@
+package test;
+
+import java.util.Date;
+import java.util.Scanner;
+
+public class etc0406 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Date date = new Date();
+
+        System.out.println(date.toString());
+    }
+}
