650a54a0edbffa5a42ec6d3cf55fb12000418498
YoungH66 <rladudgns2222@gmail.com>
2024-04-10 23:53:58 +0900

    0410

---
+++ testtest/src/main/java/test/etc0410.java
@@ -0,0 +1,14 @@
+package test;
+
+import java.util.Scanner;
+
+public class etc0410 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("오늘의 기분은?");
+        String feel = sc.nextLine();
+
+        System.out.println("오늘은 기분은 ["+feel+"] 이군요!");
+
+    }
+}
