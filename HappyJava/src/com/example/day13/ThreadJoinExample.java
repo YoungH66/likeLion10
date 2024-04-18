8c91b303535c06ca06e088ccf9ad684962abc4eb
YoungH66 <rladudgns2222@gmail.com>
2024-04-18 15:15:16 +0900

    0418 MultiThread update 04

--- HappyJava/src/com/example/day13/ThreadJoinExample.java
+++ HappyJava/src/com/example/day13/ThreadJoinExample.java
@@ -1,5 +1,18 @@
 package com.example.day13;
 
+class DemonThread extends Thread {
+    @Override
+    public void run() {
+        while(true){
+            System.out.println("Now playing Background music...");
+            try {
+                sleep(1000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+}
 public class ThreadJoinExample {
     static class TaskThread extends Thread {
         private String taskName;
@@ -21,8 +34,16 @@ public class ThreadJoinExample {
     public static void main(String[] args) {
         TaskThread t1 = new TaskThread("t1");
         TaskThread t2 = new TaskThread("t2");
+
+        DemonThread dt = new DemonThread();
+
         t1.start();
         t2.start();
+
+        // 데몬스레드로 사용한다고 함
+        // 원래는 스레드가 종료되지 않지만, 데몬스레드로 사용하면 주 스레드가 끝남에 따라 종료됨.
+        dt.setDaemon(true);
+        dt.start();
         try{
             System.out.println("Waiting for task to finish");
             t1.join();
