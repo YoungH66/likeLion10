4d06a8e6de541253a31e654f834960fd74ed52c5
YoungH66 <rladudgns2222@gmail.com>
2024-05-17 17:04:23 +0900

    0517 update

--- springmvc/src/main/java/org/example/springmvc/controller/MyController.java
+++ springmvc/src/main/java/org/example/springmvc/controller/MyController.java
@@ -1,8 +1,12 @@
 package org.example.springmvc.controller;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.thymeleaf.spring6.SpringTemplateEngine;
+import org.thymeleaf.spring6.view.ThymeleafViewResolver;
 
 @Controller
 //@RestController
@@ -11,4 +15,31 @@ public class MyController {
     public String name(){
         return "home";
     }
+
+    @GetMapping("/about")
+    public String about(){
+        return "about";
+    }
+
+    @GetMapping("/contact")
+    public String contact(){
+        return "contact";
+    }
+
+//    @Bean
+//    public InternalResourceViewResolver internalResourceViewResolver(){
+//        InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+//        resolver.setPrefix("/WEB-INF/views/");
+//        resolver.setSuffix(".jsp");
+//        resolver.setOrder(1);
+//        return resolver;
+//    }
+//
+//    @Bean
+//    public ThymeleafViewResolver thymeleafViewResolver(SpringTemplateEngine templateEngine){
+//        ThymeleafViewResolver resolver = new ThymeleafViewResolver();
+//        resolver.setTemplateEngine(templateEngine);
+//        resolver.setOrder(2);
+//        return resolver;
+//    }
 }
