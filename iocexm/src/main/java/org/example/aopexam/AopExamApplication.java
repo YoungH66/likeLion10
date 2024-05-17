5e229d4fb4795ecd6433278e38d24c6822a09261
YoungH66 <rladudgns2222@gmail.com>
2024-05-17 10:50:05 +0900

    0517 update

---
+++ iocexm/src/main/java/org/example/aopexam/AopExamApplication.java
@@ -0,0 +1,25 @@
+package org.example.aopexam;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class AopExamApplication implements CommandLineRunner {
+    public static void main(String[] args) {
+        SpringApplication.run(AopExamApplication.class, args);
+    }
+
+    @Autowired
+    SimpleService service;
+
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println("run test..");
+        System.out.println(service.doSomething());
+        
+        service.hello();
+    }
+}
---
+++ iocexm/src/main/java/org/example/aopexam/SimpleService.java
@@ -0,0 +1,16 @@
+package org.example.aopexam;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class SimpleService {
+    public String doSomething() {
+        // 핵심 관심. target
+        System.out.println("SimpleService doSomething run..");
+        return "Doing something...";
+    }
+
+    public void hello(){
+        System.out.println("hello method run :::::::");
+    }
+}
