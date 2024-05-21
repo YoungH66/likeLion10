9c0c662af415ee467b531e7a6a45a7e8a5a407b1
YoungH66 <rladudgns2222@gmail.com>
2024-05-21 17:59:19 +0900

    0521 test
    StringTokenizer 사용하도록 수정

--- testtest/src/main/java/test/test0521.java
+++ testtest/src/main/java/test/test0521.java
@@ -1,19 +1,23 @@
 package test;
 
 import java.io.*;
+import java.util.StringTokenizer;
 
 public class test0521 {
     public static void main(String[] args) {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        try {
+
+        try ( BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        ){
+            // StringTokenizer 사용.
+            // 빠름
+            StringTokenizer st = new StringTokenizer(br.readLine());
             int t = Integer.parseInt(br.readLine());
             for(int i = 0; i < t; i++) {
                 int n = Integer.parseInt(br.readLine());
                 int maxNum = -1000000;
                 int minNum = 1000000;
-                String[] line = br.readLine().split(" ");
                 for(int j = 0; j < n; j++) {
-                    int num = Integer.parseInt(line[j]);
+                    int num = Integer.parseInt(st.nextToken());
                     if(num > maxNum) {
                         maxNum = num;
                     }
