b503470949f5f6ba20a70243c950bfd38dfe2ed2
YoungH66 <rladudgns2222@gmail.com>
2024-05-23 14:27:40 +0900

    0523 update

--- friendexam/src/main/java/org/example/friendexam/controller/FriendController.java
+++ friendexam/src/main/java/org/example/friendexam/controller/FriendController.java
@@ -5,8 +5,8 @@ import org.example.friendexam.domain.Friend;
 import org.example.friendexam.service.FriendService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequiredArgsConstructor
@@ -21,5 +21,30 @@ public class FriendController {
         return "friends/list";
     }
 
+    // 친구 등록 - url 몇개? -- addForm add   -- 메소드별로 따로 일
+    // friends/add  Get Post
+    @GetMapping("/add")
+    public String addForm(Model model){
+        model.addAttribute("friend", new Friend());
+        return "friends/form";
+    }
 
+    @PostMapping("/add")
+    public String addFriend(@ModelAttribute Friend friend,
+                            RedirectAttributes redirectAttributes){
+        friendService.saveFriend(friend);
+        redirectAttributes.addFlashAttribute("message", "add successfully");
+        return "redirect:/friends";
+    }
+
+    // 상세 페이지
+    // localhost/friends/1
+    @GetMapping("/{id}")
+    public String detailFriend(@PathVariable Long id, Model model){
+        Friend friend = friendService.findFriendById(id);
+        model.addAttribute("friend", friend);
+        return "friends/detail";
+    }
+
+    
 }
