3e14b7ba9ab8e67a40d65b1f7ce8859cbfe009fc
YoungH66 <rladudgns2222@gmail.com>
2024-06-03 13:48:30 +0900

    0603 update

--- restexam/src/main/java/org/example/restexam/controller/TodoController.java
+++ restexam/src/main/java/org/example/restexam/controller/TodoController.java
@@ -3,9 +3,7 @@ package org.example.restexam.controller;
 import lombok.RequiredArgsConstructor;
 import org.example.restexam.domain.Todo;
 import org.example.restexam.service.TodoService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -18,5 +16,32 @@ public class TodoController {
     @GetMapping
     public List<Todo> getTodos() {
         return todoService.getTodos();
+    }
+
+    @GetMapping("/{id}")
+    public Todo getTodo(@PathVariable("id") Long id){
+        return todoService.getTodo(id);
+    }
+
+    @PostMapping
+    public Todo addTodo(@RequestBody Todo todo){
+        return todoService.addTodo(todo.getTodo());
+    }
+
+    // put vs patch -- 수정 || put은 전제 내용을 수정할때, patch는 일부를 수정할 때
+    @PatchMapping("/{id}")
+    public Todo updateById(@PathVariable("id") Long id){
+        return todoService.updateTodo(id);
+    }
+
+    @PatchMapping
+    public Todo updateTodo(@RequestBody Todo todo){
+        return todoService.updateTodo(todo);
+    }
+
+    @DeleteMapping
+    public String deleteTodo(@PathVariable("id") Long id){
+        todoService.deleteTodo(id);
+        return "ok";
     }
 }
