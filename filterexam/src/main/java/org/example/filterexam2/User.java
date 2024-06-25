7d1a97b2c2eb11c85df97fbba67d638e122b90a3
YoungH66 <rladudgns2222@gmail.com>
2024-06-25 10:38:16 +0900

    0625 update
    filterexam2 폴더 추가

---
+++ filterexam/src/main/java/org/example/filterexam2/User.java
@@ -0,0 +1,12 @@
+package org.example.filterexam2;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter@Setter
+@ToString
+public class User {
+    private String username;
+    private String password;
+}
