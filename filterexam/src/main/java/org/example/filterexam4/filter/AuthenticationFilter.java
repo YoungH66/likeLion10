1b698d66a54b929c15a1553312da8749c67740c6
YoungH66 <rladudgns2222@gmail.com>
2024-06-26 10:27:49 +0900

    0626 update
    인증을 위해 config에 주입하는 내용 추가

--- filterexam/src/main/java/org/example/filterexam4/filter/AuthenticationFilter.java
+++ filterexam/src/main/java/org/example/filterexam4/filter/AuthenticationFilter.java
@@ -3,16 +3,22 @@ package org.example.filterexam4.filter;
 import jakarta.servlet.*;
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
+import lombok.Setter;
 import org.example.filterexam4.entity.User;
+import org.example.filterexam4.service.UserService;
 
 
 import java.io.IOException;
 
+@Setter
 public class AuthenticationFilter implements Filter {
+    private UserService userService;
+
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
         try {
             HttpServletRequest request = (HttpServletRequest) servletRequest;
+            String path = request.getRequestURI();  // 요청주소 ex. /welcome, /loginform ...
             String auth = null;
             Cookie[] cookies = request.getCookies();
             if (cookies != null) {
@@ -23,7 +29,10 @@ public class AuthenticationFilter implements Filter {
                     }
                 }
             }
+            // 인증된 사용자라면
             if(auth != null) {
+                // 요청 url 에 따라서 http://localhost8080/admin -- Role 이 ROLE_ADMIN 인 사용자에게만 인가되도록
+
                 User user = new User();
                 user.setUsername(auth);
 
