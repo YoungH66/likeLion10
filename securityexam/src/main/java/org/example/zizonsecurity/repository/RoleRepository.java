58010a4a49dc864480e1fd2809cf9362d593cf77
YoungH66 <rladudgns2222@gmail.com>
2024-06-27 11:56:19 +0900

    0627 update

---
+++ securityexam/src/main/java/org/example/zizonsecurity/repository/RoleRepository.java
@@ -0,0 +1,8 @@
+package org.example.zizonsecurity.repository;
+
+import org.example.zizonsecurity.domain.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoleRepository extends JpaRepository<Role, Long>{
+
+}
