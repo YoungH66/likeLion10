6b1376a6e545256a9bb9291270149f635bf7fd9d
YoungH66 <rladudgns2222@gmail.com>
2024-07-01 11:59:46 +0900

    0701 update
    jwt 연습, accesstoken 생성 연습

--- securityexam/src/main/java/org/example/jwtexam/jwtApplication.java
+++ securityexam/src/main/java/org/example/jwtexam/jwtApplication.java
@@ -1,13 +1,34 @@
 package org.example.jwtexam;
 
+import org.example.jwtexam.util.JwtTokenizer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.Arrays;
 
 @SpringBootApplication
 public class jwtApplication {
+
+	private static final Logger log = LoggerFactory.getLogger(jwtApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(jwtApplication.class, args);
 	}
 
+	@Autowired
+	JwtTokenizer jwtTokenizer;
+	@Bean
+	public CommandLineRunner run(){
+		return args -> {
+			String accessToken = jwtTokenizer.createAccessToken(
+					1L, "test@test.com", "test", "testuser", Arrays.asList("ROLE_USER", "ROLE_ADMIN")
+			);
+			log.info("AccessToken {}", accessToken);
+		};
+	}
 }
---
+++ securityexam/src/main/java/org/example/jwtexam/util/JwtTokenizer.java
@@ -0,0 +1,83 @@
+package org.example.jwtexam.util;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.nio.charset.StandardCharsets;
+import java.security.Key;
+import java.util.Date;
+import java.util.List;
+
+@Component
+@Slf4j
+public class JwtTokenizer {
+    private final byte[] accessSecret;
+    private final byte[] refreshSecret;
+
+    public static Long ACCESS_TOKEN_EXPIRATION_TIME = 30 * 60 * 1000L;  // 30min.
+    public static Long REFRESH_TOKEN_EXPIRATION_TIME = 7 * 24 * 60 * 60 * 1000L; // 7d.
+
+    public JwtTokenizer(@Value("${jwt.secretKey}") String accessSecret, @Value("${jwt.refreshKey}")String refreshSecret) {
+        this.accessSecret = accessSecret.getBytes(StandardCharsets.UTF_8);  // byte 배열이니까 String으로 받아온걸 byte로 저장
+        this.refreshSecret = refreshSecret.getBytes(StandardCharsets.UTF_8);
+    }
+
+    private String createToken(Long id, String email, String name, String username,
+                               List<String> roles, Long expire, byte[] sKey){
+        Claims claims = Jwts.claims().setSubject(email);
+
+        // 필요한 정보들을 저장
+        claims.put("username", username);
+        claims.put("name", name);
+        claims.put("userId", id);
+        claims.put("roles", roles);
+        return Jwts.builder()
+                .setClaims(claims)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(new Date().getTime()+expire))
+                .signWith(getSigningKey(sKey))
+                .compact();
+    }
+
+    public static Key getSigningKey(byte[] secretKey){
+        return Keys.hmacShaKeyFor(secretKey);
+    }
+
+    //ACCESS TOKEN 생성
+    public String createAccessToken(Long id, String email, String name, String username,
+                                    List<String> roles){
+        return createToken(id, email, name, username, roles, ACCESS_TOKEN_EXPIRATION_TIME, accessSecret);
+    }
+    //REFRESH TOKEN 생성
+    public String createRefreshToken(Long id, String email, String name, String username, List<String> roles){
+        return createToken(id, email, name, username, roles, REFRESH_TOKEN_EXPIRATION_TIME, refreshSecret);
+    }
+
+    public Long getUserIdFromToken(String token){
+        String[] tokenArr = token.split(" ");
+        token = tokenArr[1];
+        Claims claims = parseToken(token,accessSecret);
+        return Long.valueOf((Integer)claims.get("userId"));
+    }
+
+    public Claims parseToken(String token, byte[] secretKey){
+        return Jwts.parserBuilder()
+                .setSigningKey(getSigningKey(secretKey))
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+
+    public Claims parseAccessToken(String accessToken) {
+        return parseToken(accessToken, accessSecret);
+    }
+
+    public Claims parseRefreshToken(String refreshToken) {
+        return parseToken(refreshToken, refreshSecret);
+    }
+}
