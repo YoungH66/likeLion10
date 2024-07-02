a8d27a60d647db245f8bc8d605be1e7e50475a6a
YoungH66 <rladudgns2222@gmail.com>
2024-07-02 11:58:09 +0900

    0702 update

--- securityexam/src/main/java/org/example/jwtexam/controller/UserApiController.java
+++ securityexam/src/main/java/org/example/jwtexam/controller/UserApiController.java
@@ -15,6 +15,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -84,5 +85,10 @@ public class UserApiController {
         response.addCookie(refreshTokenCookie);
 
         return new ResponseEntity(loginResponseDto, HttpStatus.OK);
+    }
+
+    @GetMapping("/api/authtest")
+    public String authTest(){
+        return "authTest";
     }
 }
