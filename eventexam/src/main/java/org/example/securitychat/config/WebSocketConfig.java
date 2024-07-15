b6381d78a7517c5744ae5635fdb80cb8fea6bd2a
YoungH66 <rladudgns2222@gmail.com>
2024-07-15 14:03:01 +0900

    0715 update
    import 잘못잡고 있었음

--- eventexam/src/main/java/org/example/securitychat/config/WebSocketConfig.java
+++ eventexam/src/main/java/org/example/securitychat/config/WebSocketConfig.java
@@ -2,7 +2,7 @@ package org.example.securitychat.config;
 
 import lombok.RequiredArgsConstructor;
 import org.example.securitychat.websocket.CustomHandshakeInterceptor;
-import org.example.websocketchat.MyWebSocketHandler;
+import org.example.securitychat.websocket.MyWebSocketHandler;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
@@ -15,6 +15,7 @@ public class WebSocketConfig implements WebSocketConfigurer {
     private final CustomHandshakeInterceptor customHandshakeInterceptor;
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
-        registry.addHandler(new MyWebSocketHandler(), "/ws").setAllowedOrigins("*");
+        registry.addHandler(new MyWebSocketHandler(), "/ws").setAllowedOrigins("*")
+                .addInterceptors(customHandshakeInterceptor);
     }
 }
