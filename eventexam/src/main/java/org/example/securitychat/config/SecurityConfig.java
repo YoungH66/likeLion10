f4fb927f0e454c01032b95565cd22a58624a7886
YoungH66 <rladudgns2222@gmail.com>
2024-07-15 11:35:25 +0900

    0715 update

--- eventexam/src/main/java/org/example/securitychat/config/SecurityConfig.java
+++ eventexam/src/main/java/org/example/securitychat/config/SecurityConfig.java
@@ -1,14 +1,36 @@
 package org.example.securitychat.config;
 
+import lombok.RequiredArgsConstructor;
+import org.example.securitychat.security.CustomUserDetailsService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+    private final CustomUserDetailsService userDetailsService;
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests(authorize -> authorize
+                        .requestMatchers("/userregform", "/userreg", "/css/**", "/js/**").permitAll()
+                        .requestMatchers("/ws", "/chat").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .formLogin(Customizer.withDefaults())
+                .userDetailsService(userDetailsService)
+                .csrf(csrf -> csrf.disable());
+
+        return http.build();
+    }
+
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
